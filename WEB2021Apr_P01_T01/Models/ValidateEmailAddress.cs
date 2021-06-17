64a0a1201a321c6ae2b6e58144f849dce20d732c
Villicus-np <s10207947@connect.np.edu.sg>
2021-06-17 22:02:16 +0800

    Commented out entire class to be done later

--- WEB2021Apr_P01_T01/Models/ValidateEmailAddress.cs
+++ WEB2021Apr_P01_T01/Models/ValidateEmailAddress.cs
@@ -10,24 +10,27 @@ namespace WEB2021Apr_P01_T01.Models
     public class ValidateEmailAddress : ValidationAttribute
     {
         // Change later need database
-        private LoginDAL loginContext = new LoginDAL();
-
+        private JudgeDAL judgeContext = new JudgeDAL();
+        /*
         protected override ValidationResult IsValid(
         object value, ValidationContext validationContext)
         {
             // Get the email value to validate
             string email = Convert.ToString(value);
+
             // Casting the validation context to the "Staff" model class
-            Staff staff = (Staff)validationContext.ObjectInstance;
+            Login login = (Login)validationContext.ObjectInstance;
             // Get the Staff Id from the staff instance
+            /*
             int staffId = staff.StaffId;
-            if (staffContext.IsEmailExist(email, staffId))
+            if (judgeContext.IsEmailExist(email, staffId))
                 // validation failed
                 return new ValidationResult
                 ("Email address already exists!");
             else
                 // validation passed
                 return ValidationResult.Success;
-        }
+           
+        }*/
     }
 }
