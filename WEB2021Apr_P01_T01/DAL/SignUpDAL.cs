cca164879b2ce0be25c5f6271326b2ec72a2ed41
JiaYongg <s10202579@connect.np.edu.sg>
2021-06-17 22:20:33 +0800

    Implemented AddCompetitor method in SignUpDAL

--- WEB2021Apr_P01_T01/DAL/SignUpDAL.cs
+++ WEB2021Apr_P01_T01/DAL/SignUpDAL.cs
@@ -28,5 +28,30 @@ namespace WEB2021Apr_P01_T01.DAL
             conn = new SqlConnection(strConn);
         }
 
+        public int AddCompetitor(Competitor competitor)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = @"INSERT INTO Competitor (CompetitorName, Salutation, EmailAddr, Password) 
+            OUTPUT INSERTED.CompetitorId VALUES(@name, @salutation, @email, @password)";
+
+            cmd.Parameters.AddWithValue("@name", competitor.Name);
+            cmd.Parameters.AddWithValue("@salutation", competitor.Salutation);
+            cmd.Parameters.AddWithValue("@email", competitor.EmailAddr);
+            cmd.Parameters.AddWithValue("@password", competitor.Password);
+
+            conn.Open();
+
+            competitor.CompetitorId = (int)cmd.ExecuteScalar();
+
+            conn.Close();
+
+            return competitor.CompetitorId;
+        }
+
+        public int AddJudge()
+        {
+
+        }
     }
 }
