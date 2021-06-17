0ed41556278d92ced188c64de00d162ef61d7bb7
JiaYongg <s10202579@connect.np.edu.sg>
2021-06-17 21:57:34 +0800

    Added AreaInterest, Competitor, Judge Model Classes

---
+++ WEB2021Apr_P01_T01/Models/Competitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEB2021Apr_P01_T01.Models
+{
+    public class Competitor
+    {
+        [Required]
+        [Display(Name = "ID")]
+        public int CompetitorId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(5)]
+        public string? Salutation { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string EmailAddr { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+    }
+}
---
+++ WEB2021Apr_P01_T01/Models/Judge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB2021Apr_P01_T01.Models
+{
+    public class Judge
+    {
+        [Required]
+        [Display(Name = "ID")]
+        public int JudgeId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(5)]
+        public string? Salutation { get; set; }
+
+        public int AreaInterestId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string EmailAddr { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+    }
+}
