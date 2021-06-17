d958f74592f01852546632b8db0683551235dda0
JiaYongg <s10202579@connect.np.edu.sg>
2021-06-17 21:58:17 +0800

    Implemented SignUpDAL halfway

--- WEB2021Apr_P01_T01/DAL/SignUpDAL.cs
+++ WEB2021Apr_P01_T01/DAL/SignUpDAL.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WEB2021Apr_P01_T01.Models;
@@ -15,7 +16,17 @@ namespace WEB2021Apr_P01_T01.DAL
         //Constructor
         public SignUpDAL()
         {
+            // Read ConnectionString from appsetting.json file
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
 
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString("CJP_DBConnectionString");
+
+            // Instantiate a SqlConnection object with the Connection String read.
+            conn = new SqlConnection(strConn);
         }
+
     }
 }
