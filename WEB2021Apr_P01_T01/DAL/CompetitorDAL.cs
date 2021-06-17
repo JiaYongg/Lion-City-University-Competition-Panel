86f7c5637a74bd45d9f3fa484e8bbbcac2fa8193
Villicus-np <s10207947@connect.np.edu.sg>
2021-06-17 22:03:34 +0800

    Added in DAL GetAll class

--- WEB2021Apr_P01_T01/DAL/CompetitorDAL.cs
+++ WEB2021Apr_P01_T01/DAL/CompetitorDAL.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WEB2021Apr_P01_T01.Models;
@@ -15,7 +16,42 @@ namespace WEB2021Apr_P01_T01.DAL
         //Constructor
         public CompetitorDAL()
         {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString("CJP_DBConnectionString");
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
 
+        public List<Competitor> GetAllCompetitor()
+        {
+            //Create a SqlCommand object from connection object
+            SqlCommand cmd = conn.CreateCommand();
+            //Specify the SELECT SQL statement
+            cmd.CommandText = @"SELECT * FROM Competitor ORDER BY ComepetitorID";
+            //Open a database connection
+            conn.Open();
+            //Execute the SELECT SQL through a DataReader
+            SqlDataReader reader = cmd.ExecuteReader();
+            //Read all records until the end, save data into a staff list
+            List<Competitor> staffList = new List<Competitor>();
+            while (reader.Read())
+            {
+                staffList.Add(
+                new Competitor
+                {
+                    CompetitorId = reader.GetInt32(0), 
+                    Name = reader.GetString(1),
+                    Salutation = !reader.IsDBNull(2) ? reader.GetString(2) : null,
+                    EmailAddr = reader.GetString(3), 
+                    Password = reader.GetString(4), 
+                }
+                );
+            }
         }
     }
 }
--- WEB2021Apr_P01_T01/DAL/JudgeDAL.cs
+++ WEB2021Apr_P01_T01/DAL/JudgeDAL.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WEB2021Apr_P01_T01.Models;
@@ -15,7 +16,43 @@ namespace WEB2021Apr_P01_T01.DAL
         //Constructor
         public JudgeDAL()
         {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString("CJP_DBConnectionString");
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
 
+        public List<Judge> GetAllCompetitor()
+        {
+            //Create a SqlCommand object from connection object
+            SqlCommand cmd = conn.CreateCommand();
+            //Specify the SELECT SQL statement
+            cmd.CommandText = @"SELECT * FROM Judge ORDER BY JudgeID";
+            //Open a database connection
+            conn.Open();
+            //Execute the SELECT SQL through a DataReader
+            SqlDataReader reader = cmd.ExecuteReader();
+            //Read all records until the end, save data into a staff list
+            List<Judge> staffList = new List<Judge>();
+            while (reader.Read())
+            {
+                staffList.Add(
+                new Judge
+                {
+                    JudgeId = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Salutation = !reader.IsDBNull(2) ? reader.GetString(2) : null,
+                    AreaInterestId = reader.GetInt32(3),
+                    EmailAddr = reader.GetString(4),
+                    Password = reader.GetString(5),
+                }
+                );
+            }
         }
     }
 }
