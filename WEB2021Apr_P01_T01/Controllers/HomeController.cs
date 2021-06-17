a07e2e8b26d129852ae82af9bbe26ef19731ea13
Villicus-np <s10207947@connect.np.edu.sg>
2021-06-17 22:03:02 +0800

    Added admin login - semi completed

--- WEB2021Apr_P01_T01/Controllers/HomeController.cs
+++ WEB2021Apr_P01_T01/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,46 @@ namespace WEB2021Apr_P01_T01.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult StaffLogin(IFormCollection formData)
+        {
+            // Read inputs from textboxes
+            // Email address converted to lowercase
+            string loginID = formData["adminID"].ToString().ToLower();
+            string password = formData["adminPassword"].ToString();
+            if (loginID == "admin@np.com" && password == "password1234")
+            {
+                // Admin login
+                // Store Login ID in session with the key “LoginID”
+                HttpContext.Session.SetString("adminID", loginID);
+
+                // Check what needs to be done here
+                // Store user role “Staff” as a string in session with the key “Role”
+                HttpContext.Session.SetString("Role", "Staff");
+
+                // Redirect user to admin panel.
+                return RedirectToAction("*");
+            }
+            else
+            {
+                // Store an error message in TempData for display at the index view
+                TempData["Message"] = "Invalid Login Credentials!";
+
+                // Redirect user back to the index view through an action
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult StaffMain()
+        {
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
