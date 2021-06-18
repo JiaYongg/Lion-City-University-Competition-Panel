60bffd3c1e30b314caf809d55bae798ebc48a903
JiaYongg <s10202579@connect.np.edu.sg>
2021-06-18 11:34:52 +0800

    Fully implemented SignUp Model

--- WEB2021Apr_P01_T01/Models/SignUp.cs
+++ WEB2021Apr_P01_T01/Models/SignUp.cs
@@ -9,22 +9,30 @@ namespace WEB2021Apr_P01_T01.Models
     public class SignUp
     {
         [Display(Name = "First Name")]
-        [Required(ErrorMessage = "Please enter a first name")]
+        [Required(ErrorMessage = "This field is required.")]
         public string firstName { get; set; }
         
         [Display(Name = "Last Name")]
-        [Required(ErrorMessage = "Please enter a last name")]
+        [Required(ErrorMessage = "This field is required.")]
         public string lastName { get; set; }
         [Display(Name = "Email Address")]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")] // <- [EmailAddress]
-        [Required(ErrorMessage = "Please enter a valid email address")]
+        [Required(ErrorMessage = "This field is required.")]
         [ValidateEmailAddress]
         public string emailAddress { get; set; }
 
         [Display(Name = "Password")]
-        [Required(ErrorMessage = "Please enter a last name")]
+        [Required(ErrorMessage = "This field is required.")]
         [DataType(DataType.Password)]
         public string password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "This field is required.")]
+        [Compare("password")]
+        [DataType(DataType.Password)]
+        public string confirmPassword { get; set; }
+
+        public string userType { get; set; }
 
     }
 }
