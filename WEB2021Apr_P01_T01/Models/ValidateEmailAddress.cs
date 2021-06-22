fef6187b02ef1107270f36ae84db6363f7280400
JiaYongg <s10202579@connect.np.edu.sg>
2021-06-22 19:28:44 +0800

    Commented out validation

--- WEB2021Apr_P01_T01/Models/ValidateEmailAddress.cs
+++ WEB2021Apr_P01_T01/Models/ValidateEmailAddress.cs
@@ -11,31 +11,31 @@ namespace WEB2021Apr_P01_T01.Models
     {
         private SignUpDAL signUpContext = new SignUpDAL();
 
-        protected override ValidationResult IsValid(
-        object value, ValidationContext validationContext)
-        {
-            // Get the email value to validate
-            string email = Convert.ToString(value);
+        //protected override ValidationResult IsValid(
+        //object value, ValidationContext validationContext)
+        //{
+        //    // Get the email value to validate
+        //    string email = Convert.ToString(value);
 
-            // Casting the validation context to the "Staff" model class
-            Competitor competitor = (Competitor)validationContext.ObjectInstance;
-            Judge judge = (Judge)validationContext.ObjectInstance;
+        //    // Casting the validation context to the "Competitor" model class
+        //    Competitor competitor = (Competitor)validationContext.ObjectInstance;
+        //    Judge judge = (Judge)validationContext.ObjectInstance;
 
-            // Get the Staff Id from the staff instance
-            int competitorId = competitor.CompetitorId;
-            int judgeId = judge.JudgeId;
+        //    // Get the Staff Id from the staff instance
+        //    int competitorId = competitor.CompetitorId;
+        //    int judgeId = judge.JudgeId;
 
-            if (signUpContext.IsCompetitorEmailExist(email, competitorId))
-                // validation failed
-                return new ValidationResult
-                ("Email address already exists!");
-            else if (signUpContext.IsJudgeEmailExist(email, judgeId))
-                // validation failed
-                return new ValidationResult
-                ("Email address already exists!");
-            else
-                // validation passed
-                return ValidationResult.Success;
-        }
+        //    if (signUpContext.IsCompetitorEmailExist(email, competitorId))
+        //        // validation failed
+        //        return new ValidationResult
+        //        ("Email address already exists!");
+        //    else if (signUpContext.IsJudgeEmailExist(email, judgeId))
+        //        // validation failed
+        //        return new ValidationResult
+        //        ("Email address already exists!");
+        //    else
+        //        // validation passed
+        //        return ValidationResult.Success;
+        //}
     }
 }
