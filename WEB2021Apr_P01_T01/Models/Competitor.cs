704a72d995a01df79c33a3b392dfe4e5f8dc50af
JiaYongg <s10202579@connect.np.edu.sg>
2021-06-22 19:39:56 +0800

    Revert "Revert "Added validation for email in Competitor model""
    This reverts commit d277cbfb9e351d41158e75f93775beddcc1af6d6.

--- WEB2021Apr_P01_T01/Models/Competitor.cs
+++ WEB2021Apr_P01_T01/Models/Competitor.cs
@@ -19,8 +19,9 @@ namespace WEB2021Apr_P01_T01.Models
         [StringLength(5)]
         public string? Salutation { get; set; }
 
-        [Required]
-        [StringLength(50)]
+        [Display(Name = "Email Address")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")] // <- [EmailAddress]
+        [Required(ErrorMessage = "This field is required.")]
         public string EmailAddr { get; set; }
 
         [Required]
