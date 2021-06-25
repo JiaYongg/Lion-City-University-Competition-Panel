3e0382ceeea39a14620a4d88e81956e23048b38f
Jia Yong <s10202579@connect.np.edu.sg>
2021-06-25 11:55:27 +0800

    Partially implemented Competitor Panel

---
+++ WEB2021Apr_P01_T01/Controllers/CompetitorController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB2021Apr_P01_T01.Models;
+
+namespace WEB2021Apr_P01_T01.Controllers
+{
+    public class CompetitorController : Controller
+    {
+        private Competitor competitor = new Competitor();
+        // GET: CompetitorController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CompetitorController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CompetitorController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CompetitorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CompetitorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CompetitorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CompetitorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CompetitorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
